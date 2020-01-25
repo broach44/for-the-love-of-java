dd08f2134432f432f98768eda4cb7122584ff962
Crystal Broach <crysland@gmail.com>
2020-01-25 11:34:20 -0600

    read user profile complete

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import authData from '../../../helpers/data/authData';
+import userProfileData from '../../../helpers/data/userProfileData';
+
 import './Profile.scss';
 
 class Profile extends React.Component {
+  state = {
+    user: {},
+  }
+
+  componentDidMount() {
+    const uid = authData.getUid();
+    userProfileData.getProfileByUid(uid)
+      .then((user) => {
+        const newUser = user[0];
+        this.setState({ user: newUser });
+      })
+      .catch((err) => console.error('err from get User Profile', err));
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <div className="Profile">
-        <h1>Profile</h1>
+        <h1>User Profile</h1>
+        <h3>Name: {user.firsName} {user.lastName}</h3>
+        <h4>Location: {user.city}, {user.state}</h4>
       </div>
     );
   }
